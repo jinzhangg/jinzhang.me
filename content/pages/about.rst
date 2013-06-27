4e713e56f09c7132afd2d76ae22c2c55dff899b6
Jin Zhang <jinzhang273@gmail.com>
2013-06-26 19:37:50 -0700

    first commit

---
+++ content/pages/about.rst
@@ -0,0 +1,15 @@
+About
+########
+:date: 2013-06-26
+:author: Jin Zhang
+
+Education
+---------
+2009 B.S. Bioinformatics at University of California San Diego
+
+Skills
+------
+Python Django Flask HTML LESS CSS Javascript Linux Git SQL
+
+Project
+-------
---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Jin Zhang'
+SITENAME = u'Jin Zhang'
+SITEURL = 'http://localhost:8000'
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Github', 'http://github.com/jinzhangg'),
+)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME = 'pelican-svbtle'
+
+AUTHOR_BIO = 'Python, Django, Flask Developer'
